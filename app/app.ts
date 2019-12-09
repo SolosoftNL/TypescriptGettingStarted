e1d70c357f1ad3468e5dbfa7b2fcfbdfd45ab784
Ron <ron@Home>
2019-12-09 16:22:34 +0100

    updated till: Screenshot_2019-12-04-19-15-22-536_com.pluralsight

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,39 @@
 function startGame() {
   // starting a new game
+
+  const playerName : string | undefined = getInputValue('playername');
+  logPlayer(playerName);
+
+  postScore(100, playerName);
+  
+  
+
   var messagesElement = document.getElementById('messages');
   messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';  
 }
 
+function logPlayer(name: string = 'Player Unknown!') : void{
+  console.log(`New game starting for player: ${name}`);
+}
+
+function getInputValue(elementID: string) : string | undefined {
+  
+  const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+
+  if (inputElement.value === '') {
+    return undefined;
+  }
+  else {
+    return inputElement.value;
+  }
+}
+
+function postScore(score:number, playerName?: string): void {
+  const scoreElement : HTMLElement | null = document.getElementById('postedScores');
+  scoreElement!.innerText = `${score} - ${playerName}`;
+}
+
 document.getElementById('startGame')!.addEventListener('click', startGame);
+
+const logMessage = (message :string) => console.log(message);
+  logMessage(`Welcome to MultiMath!` );
